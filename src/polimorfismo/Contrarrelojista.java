03cbf2315594a04b68e3b8a6064ba0bb0e55b558
Guillermo18-cpu <3126810804juan@gmail.com>
2025-04-02 16:30:18 -0500

    Se creo la clase Ciclista y Contrarrelojista

---
+++ src/polimorfismo/Contrarrelojista.java
@@ -0,0 +1,29 @@
+package polimorfismo;
+
+public abstract class Contrarrelojista extends Ciclista {
+
+    private double velocidadMaxima;
+
+    public Contrarrelojista(double velocidadMaxima, int identificador, String nombre) {
+        super(identificador, nombre);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    @Override
+    public String imprimirTipo() {
+        return "Soy un ciclista";
+    }
+
+    public void imprimirDatos() {
+
+    }
+
+}
