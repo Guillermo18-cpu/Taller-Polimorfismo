60bd85b3b48bad3e0ee1fabba0cc09d6b77960ff
Guillermo18-cpu <3126810804juan@gmail.com>
2025-04-03 18:16:17 -0500

    Se creo el main en la clase Prueba

--- src/polimorfismo/Prueba.java
+++ src/polimorfismo/Prueba.java
@@ -4,14 +4,6 @@ public class Prueba {
 
     public static void main(String[] args) {
 
-        Equipo equipo = new Equipo("Postobon", "Colombia");
-        equipo.anadirCiclista(new Velocista(1, "Andres", 450, 60));
-        equipo.anadirCiclista(new Escalador(2, "Roberto", 5.2f, 12.5f));
-        equipo.anadirCiclista(new Contrarrelojista(3, "Brandon", 70));
-
-        equipo.imprimirDatosEquipo();
-        equipo.calcularTiempoTotal();
-        equipo.listarNombresCiclistas();
-        equipo.imprimirDatosCiclistaPorId(2);
-
     }
+    
+}
