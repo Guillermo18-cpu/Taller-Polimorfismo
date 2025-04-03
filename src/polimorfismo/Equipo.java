28ac611e93468aa68532b3c3375b9f878ceebb82
Santi104 <monzonedi05@gmail.com>
2025-04-03 15:49:43 -0500

    clase equipo

---
+++ src/polimorfismo/Equipo.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+public class Equipo {
+    private String nombreEquipo, pais;
+    private int tiempoTotalEquipo;
+    private List<Ciclista> ciclistas;
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public int getTiempoTotalEquipo() {
+        return tiempoTotalEquipo;
+    }
+    public void setTiempoTotalEquipo(int tiempoTotalEquipo) {
+        this.tiempoTotalEquipo = tiempoTotalEquipo;
+    }
+
+    public void añadirCiclista(Ciclista) {
+
+    }
+
+    public void imprimirDatosEquipo() {
+
+    }
+
+    public void listarNombresCiclistas() {
+
+    }
+
+    public void imprimirDatosCiclistaPorld() {
+        
+    }
+
+    public static void main(String[] args) {
+    }
+}
