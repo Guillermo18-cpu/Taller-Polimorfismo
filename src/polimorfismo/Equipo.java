a01fc0aa228ebcd3fcda66ca794794f49945ec73
Santi104 <monzonedi05@gmail.com>
2025-04-07 17:55:56 -0500

    Pequeñas modificaciones acorde a lo visual de la clase equipo

--- src/polimorfismo/Equipo.java
+++ src/polimorfismo/Equipo.java
@@ -1,12 +1,15 @@
+package polimorfismo;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Equipo {
+
     static Scanner sc = new Scanner(System.in);
 
     private String nombre;
     private String pais;
-    private int tiempoTotalEquipo= 0;
+    private int tiempoTotalEquipo = 0;
 
     public String getNombreEquipo() {
         return nombre;
@@ -52,7 +55,7 @@ public class Equipo {
             System.out.println("Tiempo acumulado: " + ciclista.getTiempoAcumulado() + "\n");
         }
     }
-    
+
     public void calcularTiempoTotal() {
         int total = 0;
         for (int i = 0; i < ciclistas.size(); i++) {
@@ -61,11 +64,11 @@ public class Equipo {
         }
         this.tiempoTotalEquipo = total;
     }
-    
-    public void imprimirDatosCiclistaPorId (int id){
-        for (int i= 0; i < ciclistas.size(); i++){
+
+    public void imprimirDatosCiclistaPorId(int id) {
+        for (int i = 0; i < ciclistas.size(); i++) {
             Ciclista ciclista = ciclistas.get(i);
-            if (ciclista.getIdentificador()== id){
+            if (ciclista.getIdentificador() == id) {
                 ciclista.imprimirDatos();
             }
         }
@@ -79,23 +82,23 @@ public class Equipo {
     }
 
     void buscarCiclista() {
-        System.out.println("Buscar Ciclista");
-        System.out.println("Digitame el codigo del ciclista que quieres buscar: ");
+        System.out.println("\nBuscar Ciclista");
+        System.out.println("Digitame el identificador del ciclista que quieres buscar: ");
         int ciclistaSearch = sc.nextInt();
 
         int validacion = 0;
         boolean bandera = false;
 
-        for (int i = 0; i < ciclistas.size(); i++) {            
+        for (int i = 0; i < ciclistas.size(); i++) {
             if (ciclistaSearch == ciclistas.get(i).getIdentificador()) {
                 validacion = i;
                 bandera = true;
-            } 
+            }
         }
 
         if (bandera == true) {
             System.out.println("El nombre del ciclista es: " + ciclistas.get(validacion).getNombre());
-        }else {
+        } else {
             System.out.println("El ciclsita no esta registrado");
         }
     }
