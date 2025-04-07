a82b56bb1ff482c43ddb696ac8119d6eba0a2a71
Santi104 <monzonedi05@gmail.com>
2025-04-07 17:57:55 -0500

    Se elimino un input erroneo en la clase

--- src/polimorfismo/Prueba.java
+++ src/polimorfismo/Prueba.java
@@ -3,7 +3,27 @@ package polimorfismo;
 public class Prueba {
 
     public static void main(String[] args) {
+        
+        Equipo equipo = new Equipo("Equipo Integrador", "Colombia");
 
+        Velocista velocista = new Velocista(400.5, 1, "Santiago");
+        velocista.setVelocidadPromedioSprint(60.0);
+        velocista.setTiempoAcumulado(120);
+
+        Escalador escalador = new Escalador(3.5f, 8.0f, 2, "Samuel");
+        escalador.setTiempoAcumulado(130);
+
+        Contrarrelojista contrarrelojista = new Contrarrelojista(70.0, 3, "Guillermo");
+        contrarrelojista.setTiempoAcumulado(115);
+
+        equipo.añadirCiclista(velocista);
+        equipo.añadirCiclista(escalador);
+        equipo.añadirCiclista(contrarrelojista);
+        equipo.calcularTiempoTotal();
+        equipo.listarNombresCiclistas();
+        equipo.buscarCiclista();
+        equipo.imprimirDatosEquipo();
+
+        System.out.println("Tiempo total del equipo: " + equipo.getTiempoTotalEquipo());
     }
-    //Hola
 }
